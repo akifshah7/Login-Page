76fe81a7bd56f082ba13007e39dd2adc3a8821a0
akifshah7 <akifshah7@gmail.com>
2023-05-17 10:54:57 +0530

    first

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC70O0utm_nuytzO1u8iEBiToT4ft8o4WY",
+    authDomain: "login-auth-3a352.firebaseapp.com",
+    projectId: "login-auth-3a352",
+    storageBucket: "login-auth-3a352.appspot.com",
+    messagingSenderId: "247542083324",
+    appId: "1:247542083324:web:2608caedcc1bf80edd7179",
+    measurementId: "G-C5WC24Z95V"
+  };
